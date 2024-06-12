562c59a136e6511f00cff565623e7a19eff06026
Ivanklanac1906 <iklanac954@gmail.com>
2024-06-12 17:35:24 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+//* selectors
+
+const ui = {
+    controls: document.getElementById("controls"),
+    drawer: document.getElementById("drawer"),
+    dismissBtn: document.getElementById("dismiss-btn")
+};
+
+// event listeners
+
+const setupEventListeners = ()  => {
+    ui.controls.addEventListener("click", showDrawer);
+    ui.dismissBtn.addEventListener("click", hideDrawer);
+};
+
+// event handlers
+
+const showDrawer = (e) => {
+    ui.drawer.classList.add("show");
+};
+
+function hideDrawer(e) {
+    ui.drawer.classList.remove("show");
+};
+
+
+
+
+
+
+setupEventListeners();
